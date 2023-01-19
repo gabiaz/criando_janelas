541e8d037b3033c451e085b698c3168813b80f67
Gabrielle Azevedo <78125543+gabiaz@users.noreply.github.com>
2023-01-19 03:03:15 -0300

    Add files via upload

---
+++ mostrar_imagem.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import os
+
+pastaImg = os.path.dirname(__file__)
+
+oppening = Tk()
+oppening.geometry("500x400")
+oppening.configure(background="DarkMagenta")
+oppening.title("      BEM VINDO !")
+
+
+def show():
+
+    img = PhotoImage(file=pastaImg+"\\gato_fofo.png")
+    msg02 = msg01.config(image=img)
+    msg02.place(x=250, y=300)
+
+
+def end():
+    exit()
+
+
+msg01 = Label(oppening, text="Ver uma imagem fofinha? ", background="DarkGoldenRod", font=("Arial", 25, "bold"))
+msg01.pack(pady=10)
+
+button = Button(oppening, text="sim \n :)", background="DarkGoldenRod", fg="black", font="Arial",command=show)
+button.pack(pady=10)
+
+button2 = Button(oppening, text=" fechar ", background="black", fg="white", font="Arial",command=end)
+button2.pack()
+
+oppening.mainloop()
